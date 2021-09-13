430500364ae685a4542fff1041f0bacb804194da
Malik Ata <Malikmajedata@gmail.com>
2021-09-12 21:27:00 -0700

    Setting up backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 import express from "express"
 import cors from "cors"
-import restaurants from "./api/resturatns.route.js"
+import restaurants from "./api/restaurants.route.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json)
+
+// Setting the restuarant API
+app.use("/api/v1/restaurants", restaurants)
+
+// Route that does not exisit
+app.use("*", (req, res) => res.status(404).json({error: "Not found"}))
+
+
+// Export so we can import in another file
+export default app
 
