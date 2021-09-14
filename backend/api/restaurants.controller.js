853676271803f0ee7d16dadc9dd82b95dd75c5f9
Malik Ata <Malikmajedata@gmail.com>
2021-09-14 09:23:03 -0700

    Fixed files names for backend

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -2,33 +2,33 @@ import RestaurantsDOA from "../doa/restaurants.DOA.js";
 
 export default class RestaurantsController {
     static async apiGetRestaurants(req, res, next) {
-        const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) :20
-        const page = req.query.page ?parseInt(req.query.page, 10) : 0
+        const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20
+        const page = req.query.page ? parseInt(req.query.page, 10) : 0
 
         let filters = {}
-            if(req.query.cuisine){
-                filters.cuisine = req.query.cuisine
-            }else if (req.query.zipcode) {
-                filters.zipcode = req.query.zipcode
-            }else if (req.query.name){
-                filters.name = req.query.name
-            }
+        if (req.query.cuisine) {
+          filters.cuisine = req.query.cuisine
+        } else if (req.query.zipcode) {
+          filters.zipcode = req.query.zipcode
+        } else if (req.query.name) {
+          filters.name = req.query.name
+        }
 
-            const {restaurantsList, totalNumRestaurants} = await RestaurantsDOA.getRestaurants(
-                filters,
-                page,
-                restaurantsPerPage
-            )
+        const { restaurantsList, totalNumRestaurants } = await RestaurantsDOA.getRestaurants({
+            filters,
+            page,
+            restaurantsPerPage,
+          })
+
 
             let response = {
                 restaurants: restaurantsList,
                 page: page,
                 filters: filters,
                 entries_per_page: restaurantsPerPage,
-                total_results: totalNumRestaurants
-
-            }
-            res.json(response)
-
+                total_results: totalNumRestaurants,
+              }
+              res.json(response)
     }
 }
+
