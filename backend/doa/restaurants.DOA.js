15796322174c37a2b05ead72624a65353ff35571
Malik Ata <Malikmajedata@gmail.com>
2021-09-14 09:23:22 -0700

    Fixed files names for backend

--- backend/doa/restaurants.DOA.js
+++ backend/doa/restaurants.DOA.js
@@ -21,40 +21,39 @@ static async getRestaurants({
     restaurantsPerPage = 20
 } ={}) {
     let query
-    if(filters){
-                if("name" in filters) {
-            query = {$text: {$search: filters["name"] } }
-        } else if ("cuisine" in filters){
-            query = {"cuisine": {$eq: filters["cuisine"] } }
-        } else if ("zipcode" in filters){
-            query = {"address.zipcode": {$eq: filters ["zipcode"]}}
-        }
-
+    if (filters) {
+      if ("name" in filters) {
+        query = { $text: { $search: filters["name"] } }
+      } else if ("cuisine" in filters) {
+        query = { "cuisine": { $eq: filters["cuisine"] } }
+      } else if ("zipcode" in filters) {
+        query = { "address.zipcode": { $eq: filters["zipcode"] } }
+      }
     }
 
     let cursor
-    try{
-        cursor = await restaurants
-            .find(query)       
-    } catch(e) {
-        console.log(`Unable to issue find command, ${e}`)
-        return { restaurants: [], totalNumRestaurants: 0}
+    
+    try {
+      cursor = await restaurants
+        .find(query)
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`)
+      return { restaurantsList: [], totalNumRestaurants: 0 }
     }
 
-        const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page)
-        try{
-            const restaurantsList = await displayCursor.toArray()
-            const totalNumRestaurants = await restaurants.countDocuments(query)
+    const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page)
 
-            return{restaurantsList, totalNumRestaurants}
-        }catch(e) {
-            console.error(
-                `Unable to convert curso to array or problem counting documents , ${e}`
-            )
-            return {restaurantsList: [], totalNumRestaurants: 0}
-        }
+    try {
+      const restaurantsList = await displayCursor.toArray()
+      const totalNumRestaurants = await restaurants.countDocuments(query)
 
-}
-
+      return { restaurantsList, totalNumRestaurants }
+    } catch (e) {
+      console.error(
+        `Unable to convert cursor to array or problem counting documents, ${e}`,
+      )
+      return { restaurantsList: [], totalNumRestaurants: 0 }
+    }
+  }
 
 }
